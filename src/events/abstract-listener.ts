daec8243485a8090248d0b981bd17dd15f03e99c
Megan Sharon <megan@sprouted.dev>
2020-06-09 15:58:03 -0400

    Some updates

--- src/events/abstract-listener.ts
+++ src/events/abstract-listener.ts
@@ -11,7 +11,7 @@ export abstract class AbstractListener<T extends Event> {
     abstract queueGroupName: string;
     abstract onMessage(data: T['data'], msg: Message): void;
 
-    private client: Stan;
+    protected client: Stan;
     public ackWait = 5 * 1000;
     constructor(client: Stan) {
         this.client = client;
--- src/events/abstract-publisher.ts
+++ src/events/abstract-publisher.ts
@@ -7,7 +7,7 @@ interface Event {
 }
 export abstract class AbstractPublisher<T extends Event> {
     abstract subject: T['subject'];
-    private client: Stan;
+    protected client: Stan;
 
     constructor(client: Stan) {
         this.client = client;
